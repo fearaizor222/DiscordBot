ac46a48120eee897c01a3f93bea55beb159b8116
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2023-08-26 23:03:30 +0700

    remove unuse data from Fee

--- API/Fee.py
+++ API/Fee.py
@@ -5,4 +5,10 @@ def Fee(sess: requests.Session()) -> dict:
     with open('API_Endpoint.json', 'r') as API:
         API_Endpoint = json.load(API)
 
-    return sess.post(API_Endpoint['Xem_Hoc_Phi']).json()['data']
+    data = sess.post(API_Endpoint['Xem_Hoc_Phi']).json()['data'].pop('ds_hoc_phi_hoc_ky')
+    needed_data = ['ten_hoc_ky', 'hoc_phi', 'da_thu', 'con_no', 'mien_giam', 'phai_thu', 'tong_hoc_bong']
+    for obj in data:
+        for key in list(obj):
+            if key not in needed_data:
+                obj.pop(key, None)
+    return data
