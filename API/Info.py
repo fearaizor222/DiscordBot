92b49896bf845655a0c22037054c48596c36cc92
thuong <thuong5so55555@gmail.com>
2023-08-28 12:32:09 +0700

    update info.py

--- API/Info.py
+++ API/Info.py
@@ -9,4 +9,14 @@ def Info(sess: requests.Session()) -> dict:
     ma_sv = info['data']['ma_sv']
     image = sess.post(API_Endpoint['Info_Picture'], params=f'MaSV={ma_sv}').json()['data']['thong_tin_sinh_vien']['image']
     info['data'].update({'image': "data:image/png;base64," + image})
-    return info['data']
+    info = info['data']
+   
+    needed_data = ['ma_sv','ten_day_du','gioi_tinh','ngay_sinh','noi_sinh','dan_toc','ton_giao','quoc_tich','dien_thoai','email','dien_thoai2',
+                   'email2','so_cmnd','ho_khau_thuong_tru_gd','so_tk','lop','khu_vuc','doi_tuong_uu_tien','khoi','nganh','chuyen_nganh','khoa',
+                   'ba_he_dao_tao','nien_khoa','ma_truong','ten_truong','nhhk_vao','nhhk_ra','hien_dien_sv','image']
+    for key in list(info):
+        if key not in needed_data:
+            info.pop(key, None)
+
+    return info
+
