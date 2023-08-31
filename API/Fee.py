e54f410bed0e0032d9b79002a942423277adaacc
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2023-08-31 13:58:56 +0700

    change implementation

--- API/Fee.py
+++ API/Fee.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import Module.JsonKeyDeletor as JsonKeyDeletor
 
 def Fee(sess: requests.Session()) -> dict:
     with open('API_Endpoint.json', 'r') as API:
@@ -7,8 +8,7 @@ def Fee(sess: requests.Session()) -> dict:
 
     data = sess.post(API_Endpoint['Xem_Hoc_Phi']).json()['data'].pop('ds_hoc_phi_hoc_ky')
     needed_data = ['ten_hoc_ky', 'hoc_phi', 'da_thu', 'con_no', 'mien_giam', 'phai_thu', 'tong_hoc_bong']
-    for obj in data:
-        for key in list(obj):
-            if key not in needed_data:
-                obj.pop(key, None)
+
+    JsonKeyDeletor(data, needed_data)
+
     return data
--- API/Info.py
+++ API/Info.py
@@ -1,22 +1,23 @@
 import json
 import requests
+import Module.JsonKeyDeletor as JsonKeyDeletor
+
 
 def Info(sess: requests.Session()) -> dict:
     with open('API_Endpoint.json', 'r') as API:
         API_Endpoint = json.load(API)
-    
+
     info = sess.post(API_Endpoint['Xem_Info']).json()
     ma_sv = info['data']['ma_sv']
-    image = sess.post(API_Endpoint['Info_Picture'], params=f'MaSV={ma_sv}').json()['data']['thong_tin_sinh_vien']['image']
+    image = sess.post(API_Endpoint['Info_Picture'], params=f'MaSV={ma_sv}').json()[
+        'data']['thong_tin_sinh_vien']['image']
     info['data'].update({'image': "data:image/png;base64," + image})
     info = info['data']
-   
-    needed_data = ['ma_sv','ten_day_du','gioi_tinh','ngay_sinh','noi_sinh','dan_toc','ton_giao','quoc_tich','dien_thoai','email','dien_thoai2',
-                   'email2','so_cmnd','ho_khau_thuong_tru_gd','so_tk','lop','khu_vuc','doi_tuong_uu_tien','khoi','nganh','chuyen_nganh','khoa',
-                   'ba_he_dao_tao','nien_khoa','ma_truong','ten_truong','nhhk_vao','nhhk_ra','hien_dien_sv','image']
-    for key in list(info):
-        if key not in needed_data:
-            info.pop(key, None)
+
+    needed_data = ['ma_sv', 'ten_day_du', 'gioi_tinh', 'ngay_sinh', 'noi_sinh', 'dan_toc', 'ton_giao', 'quoc_tich', 'dien_thoai', 'email', 'dien_thoai2',
+                   'email2', 'so_cmnd', 'ho_khau_thuong_tru_gd', 'so_tk', 'lop', 'khu_vuc', 'doi_tuong_uu_tien', 'khoi', 'nganh', 'chuyen_nganh', 'khoa',
+                   'ba_he_dao_tao', 'nien_khoa', 'ma_truong', 'ten_truong', 'nhhk_vao', 'nhhk_ra', 'hien_dien_sv', 'image']
+
+    JsonKeyDeletor(info, needed_data)
 
     return info
-
