59ba2504d5ed6c3a274c6f21f023555399a1441f
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-02-08 23:53:49 +0700

    Update token in main.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import json
 from API import *
 from Module import *
 import lightbulb
-
-additional_data = ''
-with open('Additional_Data.json', 'r') as file:
-    additional_data = json.load(file)
+from global_config import *
 
 app = lightbulb.BotApp(
-    token=additional_data['token'],
+    token=config['token'],
     prefix='!'
 )
 
