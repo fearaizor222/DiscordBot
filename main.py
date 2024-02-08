85b3cc999003178707caa25fafd94a9c571a5a36
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-02-08 20:57:14 +0700

    Add lightbulb library and implement info command

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ import requests
 import json
 from API import *
 from Module import *
+import lightbulb
 
-with requests.Session() as sess:
-    login_data = {
-        'username': 'n21dccn165',
-        'password': '25092003',
-        'grant_type': 'password'
-    }
-    Login(sess, login_data)
+additional_data = ''
+with open('Additional_Data.json', 'r') as file:
+    additional_data = json.load(file)
 
+app = lightbulb.BotApp(
+    token=additional_data['token'],
+    prefix='!'
+)
 
+@app.command
+@lightbulb.command('info', 'return info')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx: lightbulb.Context):
+    with requests.Session() as sess:
+        login_data = {
+            'username': 'n21dccn165',
+            'password': '25092003',
+            'grant_type': 'password'
+        }
+        Login(sess, login_data)
+
+        await ctx.respond(Info(sess)['ma_sv'])
+
+app.run()
