e6e7f5293dddae47d0e532f72ccf4ad29b738022
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-02-08 19:44:23 +0700

    Update login credentials and remove unnecessary code

--- main.py
+++ main.py
@@ -5,29 +5,10 @@ from Module import *
 
 with requests.Session() as sess:
     login_data = {
-        'username': '',
-        'password': '',
+        'username': 'n21dccn165',
+        'password': '25092003',
         'grant_type': 'password'
     }
     Login(sess, login_data)
 
-    user_info = Info(sess)
-    user_score = Score(sess)
-    user_schedule = TimeTable(sess, 20231)
-    user_fee = Fee(sess)
-    user_test_shedule = ExamSchedule(sess, 20221)
 
-    with open('user_info.json', 'w', encoding='utf-8') as f:
-        json.dump(user_info, f, ensure_ascii=False, indent=4)
-
-    with open('user_score.json', 'w', encoding='utf-8') as f:
-        json.dump(user_score, f, ensure_ascii=False, indent=4)
-
-    with open('user_schedule.json', 'w', encoding='utf-8') as f:
-        json.dump(user_schedule, f, ensure_ascii=False, indent=4)
-    
-    with open('user_fee.json', 'w', encoding='utf-8') as f:
-        json.dump(user_fee, f, ensure_ascii=False, indent=4)
-
-    with open('user_test_schedule.json', 'w', encoding='utf-8') as f:
-        json.dump(user_test_shedule, f, ensure_ascii=False, indent=4)
