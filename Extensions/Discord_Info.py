654c8f311d55f52660ccf59e820226a0d5e568db
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2024-02-09 17:54:19 +0700

    Add Discord user info and whoami command

--- Extensions/Discord_Info.py
+++ Extensions/Discord_Info.py
@@ -2,6 +2,7 @@ import lightbulb
 from API import *
 import requests
 from global_config import *
+import hikari
 
 plugin = lightbulb.Plugin('Info')
 
@@ -12,10 +13,29 @@ async def thisUserInfo(ctx: lightbulb.Context):
     try:
         sess = requests.Session()
         access_token = client['Users'].find_one({'discord_id': ctx.author.id})['access_token']
-        if not access_token:
-            raise TypeError
         sess.headers['authorization'] = f'bearer {access_token}'
-        await ctx.respond(Info(sess)['ma_sv'])
+        data = Info(sess)
+        embed = hikari.Embed(
+            title=f'Thông tin sinh viên {data["ma_sv"]}',
+            description=f'**Họ và tên:** {data["ten_day_du"]}'
+        )
+        embed.set_image(data['image'])
+        await ctx.respond(embed=embed)
+        sess.close()
+    except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+        await ctx.respond('Timeout')
+    except TypeError:
+        await ctx.respond('You have not linked your account yet.')
+
+@plugin.command
+@lightbulb.command('whoami', 'Return UIS account that linked with your Discord account')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def whoami(ctx: lightbulb.Context):
+    try:
+        sess = requests.Session()
+        access_token = client['Users'].find_one({'discord_id': ctx.author.id})['access_token']
+        sess.headers['authorization'] = f'bearer {access_token}'
+        await ctx.respond(f'You are currently linked with {Info(sess)["ma_sv"]}')
         sess.close()
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
         await ctx.respond('Timeout')
